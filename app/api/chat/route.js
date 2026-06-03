bca3d46708769ca1384f56be4459af4a0c059b96
fauzimuh258-ui <fauzimuh258@gmail.com>
2026-06-03 07:48:46 +0700

    Update route.js

--- app/api/chat/route.js
+++ app/api/chat/route.js
@@ -4,7 +4,7 @@ import { validateApiKey } from "@/lib/auth";
 
 const CORS_HEADERS = {
   "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Methods": "POST, OPTIONS",
+  "Access-Control-Allow-Methods": "POST, GET, OPTIONS",
   "Access-Control-Allow-Headers": "Content-Type, x-api-key",
 };
 
